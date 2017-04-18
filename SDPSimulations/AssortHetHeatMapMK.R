ea8f6c6e75d0f46e37bbb609dab9a44631d48c04
Steven Bellan <stevenbellan@s172-20-145-h45.paws.uga.edu>
2017-04-18 14:21:42 -0400

    working on assorthetheapmapmk

--- SDPSimulations/AssortHetHeatMapMK.R
+++ SDPSimulations/AssortHetHeatMapMK.R
@@ -4,8 +4,10 @@
 ####################################################################################################
 rm(list=ls())                                  # clear workspace
 if(grepl('tacc', Sys.info()['nodename'])) setwd('/home1/02413/sbellan/DHSProject/SDPSimulations/')
-load("data files/ds.nm.all.Rdata") # country names
-load('data files/pars.arr.ac.Rdata')    # load acute phase relative hazards used to fit (in.arr[,,2])
+if(grepl('stevenbellan', Sys.info()['login'])) setwd('~/Documents/R Repos/SDPSimulations/SDPSimulations/')
+source("SimulationFunctions.R")                   # load simulation functions from script
+load("../DHSFitting/data files/ds.nm.all.Rdata") # country names
+load('../DHSFitting/data files/pars.arr.ac.Rdata')    # load acute phase relative hazards used to fit (in.arr[,,2])
 hazs <- c('bmb','bfb','bme','bfe','bmp','bfp') #  transmission coefficient names, for convenience
 nc <- 12                                       # core per simulation
 ## source('AssortHetHeatMapMK.R')
@@ -17,73 +19,46 @@ nc <- 12                                       # core per simulation
 countries <- 1:length(ds.nm)
 each.val <- 200 ##  equates to ~100,000 couples
 
-blocks.beh <- expand.grid(group = countries, het.beh=T, het.beh.sd=seq(0,3, by = .1), het.beh.cor=seq(0,1, by = .05), bmb.sc = 1)
+blocks.beh <- expand.grid(country = countries, het.beh=T, het.beh.sd=seq(0,3, by = .1), het.beh.cor=seq(0,1, by = .05), bmb.sc = 1)
 blocks.beh$bfb.sc <- blocks.beh$bme.sc <- blocks.beh$bfe.sc <- blocks.beh$bmb.sc ## all contact coefficients scaled same way
 blocks.beh$bmp.sc <- blocks.beh$bfp.sc <- 1
 blocks.beh$het.gen <- F; blocks.beh$het.gen.sd <- blocks.beh$het.gen.cor <- 0
-blocks.beh <- blocks.beh[with(blocks.beh, order(group, het.beh.sd, het.beh.cor, bmb.sc)),]
+blocks.beh <- blocks.beh[with(blocks.beh, order(country, het.beh.sd, het.beh.cor, bmb.sc)),]
 
-blocks.gen <- expand.grid(group = countries, het.gen=T, het.gen.sd=seq(0,3, by = .1), het.gen.cor=seq(0,1, by = .05), bmb.sc = 1)
+blocks.gen <- expand.grid(country = countries, het.gen=T, het.gen.sd=seq(0,3, by = .1), het.gen.cor=seq(0,1, by = .05), bmb.sc = 1)
 blocks.gen$bfb.sc <- blocks.gen$bme.sc <- blocks.gen$bfe.sc <- blocks.gen$bmb.sc ## all contact coefficients scaled same way
 blocks.gen$bmp.sc <- blocks.gen$bfp.sc <- 1
 blocks.gen$het.beh <- F; blocks.gen$het.beh.sd <- blocks.gen$het.beh.cor <- 0
-blocks.gen <- blocks.gen[with(blocks.gen, order(group, het.beh.sd, het.beh.cor, bmb.sc)),]
+blocks.gen <- blocks.gen[with(blocks.gen, order(country, het.beh.sd, het.beh.cor, bmb.sc)),]
 
 blocks <- rbind(blocks.beh, blocks.gen)
-
 blocks$jobnum <- 1:nrow(blocks)
+blocksg <- data.table(blocks)
+
 nn <- nrow(blocks)
 head(blocks,50)
 
-outdir <- file.path('results','AssortHetHeatMap')
-if(!file.exists(outdir))      dir.create(outdir) # create directory if necessary
-batchdirnms <- rep(NA, length(countries))
-for(cc in 1:length(countries)) {
-    batchdirnms[cc] <- file.path(outdir, ds.nm[cc]) # setup directory for country
-    if(!file.exists(batchdirnms[cc]))      dir.create(batchdirnms[cc]) # created if necessary
-    if(!file.exists(file.path(batchdirnms[cc],'routs')))      dir.create(file.path(batchdirnms[cc], 'routs')) # setup directory to store Rout files
-}
+out.dir <- file.path('results','AssortHetHeatMap')
+blocksg[,c('group','s.epic','s.demog','scale.by.sd','scale.adj','infl.fac','maxN','sample.tmar','psNonPar','each'):= .(country,country, country, T, 1, 200, 10^5, F, F, each.val)]
+blocksg[,jobnum:=1:nrow(blocksg)]
+blocksg[,c('seed','out.dir','sim.nm','doSubs'):=.(1,out.dir, 'CF', F)]
+blocksg[,c('tmar','tint'):=.('tmar=(65*12):(113*12)',113*12)]
 
-load(file=file.path(outdir, 'JobsToDo.Rdata'))
+blocksgTD <- blocksg[country==15] ##
 
-num.doing <- 0
+if(!file.exists(out.dir))      dir.create(out.dir) # create directory if necessary
+if(!file.exists(file.path(out.dir,'Rdatas')))      dir.create(file.path(out.dir,'Rdatas')) # create directory if necessary
+if(!file.exists(file.path(out.dir,'Routs')))      dir.create(file.path(out.dir,'Routs')) # create directory if necessary
 sink("AssortHetHeatMap.txt")         # create a control file to send to the cluster
-## LEFT OFF HERE
-for(ii in 1:nn) {
-    jb <- ii                   # job num
-    cmd <- with(blocks, {
-        paste("R CMD BATCH '--args jobnum=", ii, " batchdirnm=\"", batchdirnms[group[ii]], "\"", " nc=", nc,
-              " group.ind=", group[ii], " substitute=F sub.betas=F counterf.betas=F",
-              " s.epic=", group[ii],  " s.demog=", group[ii],
-              " s.bmb=", group[ii], " s.bfb=", group[ii],
-              " s.bme=", group[ii], " s.bfe=", group[ii],
-              " s.bmp=", group[ii], " s.bfp=", group[ii], 
-              " death=T",
-              " acute.sc=7 late.sc=5 aids.sc=0 dur.ac=2 dur.lt=10 dur.aids=10", # acute phase varying throughout loop
-              " bmb.sc=", bmb.sc[ii], " bfb.sc=", bfb.sc[ii],
-              " bme.sc=", bme.sc[ii], " bfe.sc=", bfe.sc[ii],
-              " bmp.sc=", bmp.sc[ii], " bfp.sc=", bfp.sc[ii],
-              " het.b=F het.b.sd=0 het.b.cor=0",
-              " het.e=F het.e.sd=0 het.e.cor=0",
-              " het.p=F het.p.sd=0 het.p.cor=0",
-              " het.gen=", het.gen[ii], " het.gen.sd=", het.gen.sd[ii], " het.gen.cor=", het.gen.cor[ii],
-              " het.beh=", het.beh[ii], " het.beh.sd=", het.beh.sd[ii], " het.beh.cor=", het.beh.cor[ii],
-              " hilo=F phihi=.2 phi.m=.2 phi.f=.2 rrhi.m=10 rrhi.f=10", ## default hilo parameters, not used
-              " scale.by.sd=T scale.adj=1",
-              " infl.fac=200 maxN=10^5 sample.tmar=F",
-              " psNonPar=F seed=1 tmar=(65*12):(113*12) each=", each.val,
-              " tint=113*12' SimulationStarter.R ", file.path(batchdirnms[group[ii]], "routs", paste0(ds.nm[group[ii]], ii, ".Rout")), sep='')
-    })
-                                        #     if(totn %in% jtd & ii %in% 89:92 & aa==7) { ## for finishing up jobs that didn't get properly submitted (cluster issues sometimes)
-    if(blocks$group[ii]==7) { ## for finishing up jobs that didn't get properly submitted (cluster issues sometimes)
-        num.doing <- num.doing+1
-        cat(cmd)               # add command
-        cat('\n')              # add new line
-    }
+for(ii in blocksgTD[,jobnum]) { #blocksgTD[,jobnum]) {
+    cmd <- "R CMD BATCH '--no-restore --no-save --args"
+    cmd <- addParm(cmd, blocksgTD, ii) ## remove lab since it has spaces & isn't used in psrun
+    cmd <- paste0(cmd, " ' SimulationStarter.R ", file.path(out.dir,'Routs', paste0('CFsim', sprintf("%06d", ii),'.Rout')), 
+                  sep='')
+    cat(cmd)               # add command
+    cat('\n')              # add new line
 }
 sink()
-print(nn)
-print(num.doing)
-save(blocks, file = file.path(outdir,'blocks.Rdata')) # these are country-acute phase specific blocks
-####################################################################################################
-
+save(blocksg, file = file.path(out.dir,'blocksg.Rdata')) # these are country-acute phase specific blocks
+print(nrow(blocksg))
+print(nrow(blocksgTD))
