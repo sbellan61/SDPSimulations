9212426e27259ab143fb42fce10ab85c4eb605a3
Steven Bellan <stevenbellan@s172-20-246-h28.paws.uga.edu>
2017-08-17 15:25:49 -0400

    fig 2 now working from new dt version of coll()

--- SDPSimulations/CounterFactualSummaries.R
+++ SDPSimulations/CounterFactualSummaries.R
@@ -7,31 +7,20 @@ if(grepl('stevenbellan', Sys.info()['login'])) setwd('~/Documents/R Repos/SDPSim
 load("../DHSFitting/data files/dframe.s.Rdata") # country names
 source('PlotFunctions.R')                    # load functions to collect & plot results
 source('SimulationFunctions.R')                   # load simulation functions
-do.again <- F                           # collect results again (otherwise load cfs.Rdata)
 show.pts <- T                           # show observed SDP in real DHS data
 ## source('CounterFactualSummaries.R')
 dir.results <- file.path('results','CounterFactual') # results locations
 dir.figs <- file.path(dir.results, 'Figures')        # make a directory to store figures
+if(!file.exists(dir.figs)) dir.create(dir.figs) # Create directory
+load(file.path(dir.results, 'blocksg.Rdata')) ## information on simulations from MK file
+tss <- coll(dir.results, nc = 48, browse=F) ## assemble all results into one DT
+save(tss, file = file.path(dir.results, 'tss.Rdata'))
 
-if(!file.exists(dir.figs)) dir.create(dir.figs)      # create it
-## load 'blocks' which gives info on all simulations within each country-acute group
-load(file.path(dir.results, 'blocksg.Rdata'))
-## Load all results files in the results directory (all Rdata files except blocks & cfs)
-fs <- list.files(path = file.path(dir.results, 'Rdatas'), recursive = T, full.names = T)
-print(length(fs))
-
-## coll: collects all results into data frame of input parameters, and array of time series
-if(!file.exists(file.path(dir.results, 'cfs.Rdata')) | do.again) {
-    tss <- coll(dir.results, nc = 48, browse=F)
-    save(tss, file = file.path(dir.results, 'tss.Rdata'))
-}else{
-    load(file.path(dir.results, 'tss.Rdata'))
-}
-
-tss[,length(unique(jobnum))]
-dim(tss) ## dimensions & check for duplicate runs
-print(paste(tss[, sum(duplicated(jobnum))]), 'duplicated jobs')
-tss <- tss[!duplicated(jobnum)]
+tss[,tmp:=paste0(jobnum,'-',yr)] ## Any duplicated jobs X time points?
+print(paste(tss[,sum(duplicated(tmp))], 'duplicated jobs'))
+tss <- tss[!duplicated(tmp)] ## Remove them
+print(paste(tss[,length(unique(jobnum))], 'unique jobs'))
+tss$tmp <- NULL
 acutes <- tss[,unique(acute.sc)] # which acute phase relative hazards (RHs) were simulated
 nac <- length(acutes)                   # how many
 countries <- tss[,unique(country)]
@@ -41,83 +30,71 @@ jtd <- blocksg[!jobnum %in% tss$jobnum, jobnum]
 print(paste("didn't do jobs:",paste(head(jtd,50), collapse=','),', ...')) # check to see if any jobs didn't complete
 save(jtd, file=file.path(dir.results,'CFJobsToDo.Rdata'))
 
-blocks <- blocksg[jobnum %in% cframe$job, ]
-ac.to.do <- c(1) ## 1,7,25,50)
-nac <- length(ac.to.do)
-
 ####################################################################################################
 ## Figure 2 for the manuscript
 ####################################################################################################
+
 
 ## matlayout <- t(matrix(c(1:6,10,7:12,10),7,2))
 ## layout(matlayout,w = c(rep(1,6),.85))
 ## par(mar = c(3,1,2,0), oma = c(1,3,0,0), cex.lab = cx, cex.axis = cx, cex.main = cx, fg = col.pl, col.axis = col.pl,
 ##     col.lab = col.pl, col = col.pl, col.main = col.pl)
 
-indsFxn <- function(cc, ac) {
-inds <- list()
-length(inds) <- 6
-    inds[[1]] <- c(blocks[group==cc & acute.sc==ac & lab=="as fitted", jobnum]
-                 , blocks[group==cc & acute.sc==ac & lab=="no AIDS mortality, gen het=0", jobnum])
-    ## each transmission route
-    inds[[2]] <- c(blocks[group==cc & acute.sc==ac & lab=="as fitted", jobnum]
-                 , blocks[group==cc & acute.sc==ac & lab=="scale pre--couple, gen het=0" & bmb.sc %in% c(.1,10), jobnum])
-    inds[[3]] <- c(blocks[group==cc & acute.sc==ac & lab=="as fitted", jobnum]
-                 , blocks[group==cc & acute.sc==ac & lab=="scale extra--couple, gen het=0" & bme.sc %in% c(.1,10), jobnum])
-    inds[[4]] <- c(blocks[group==cc & acute.sc==ac & lab=="as fitted", jobnum]
-                 , blocks[group==cc & acute.sc==ac & lab=="scale within--couple, gen het=0" & bmp.sc %in% c(.1,10), jobnum])
-    ## heterogeneity
-    inds[[5]] <- c(blocks[group==cc & acute.sc==ac & lab=="as fitted", jobnum]
-                 , blocks[group==cc & acute.sc==ac & lab=='gen cor=0' & het.gen.sd %in% 1:2, jobnum])
-    ## assortativity (genetic, i.e. all route)
-    inds[[6]] <- c(blocks[group==cc & acute.sc==ac & lab=='gen cor=0' & het.gen.sd==2, jobnum]
-                 , blocks[group==cc & acute.sc==ac & lab=='gen cor=0.4' & het.gen.sd==2, jobnum]
-                 , blocks[group==cc & acute.sc==ac & lab=='gen cor=0.8' & het.gen.sd==2, jobnum])
-    inds
+parmsFxn <- function(country=15, s.demog=NA, death=T, acute.sc=5
+                   , bmb.sc=1, bme.sc=1, bmp.sc=1
+                   , het.b.sd=0, het.b.cor=0, het.e.sd=0, het.e.cor=0, het.p.sd=0, het.p.cor=0
+                   , het.beh.sd=0, het.beh.cor=0, het.gen.sd=0, het.gen.cor=0
+                   ) {
+    if(is.na(s.demog)) s.demog <- country
+    parms <- expand.grid(as.list(environment()))
+    return(data.table(parms))
 }
-inds <- indsFxn(cc=15, ac = 1)
-inds
+parmsFxn()
+parmsFxn(country=15, death = c(T,F))
+nms <- names(parmsFxn())
 
-mains <- c('A','B','C','D','E','F')
 mains <- c('mortality','pre-couple \ncontact coefficient','extra-couple \ncontact coefficient',
            'intrinsic \ntransmission rate', 'heterogeneity \nin transmission', 'heterogeneity \nwith assortativity')
 
-leg.cex <- .8
-cc1 <- countries
-cols <- gray(c(0,.4, .7))
+sel <-  parmsFxn()
+bases <- tss[sel, .SD, nomatch=0L, on=nms, .SDcols=names(tss)][,unique(jobnum)] ## have duplicates of base simulations based on old code
+dups <- bases[-1]
+base <- bases[1]
+sel1 <- cbind(parmsFxn(death = c(T,F)), cols = c('black','gray'), ltys = c(1,2))
+cols <- c('gray','black','brown')
+sel2 <- cbind(parmsFxn(bmb.sc = c(0,1,10)), cols = cols, ltys = c(2,1,2))
+sel3 <- cbind(parmsFxn(bme.sc = c(0,1,10)), cols = cols, ltys = c(2,1,2))
+sel4 <- cbind(parmsFxn(bmp.sc = c(.1,1,10)), cols = cols, ltys = c(2,1,2))
+cols <- c('black','gray','brown')
+sel5 <- cbind(parmsFxn(het.gen.sd = c(0,1,2)), cols = cols, ltys = c(1,2,2))
+sel6 <- cbind(parmsFxn(het.gen.sd = 2, het.gen.cor = c(0,.4,.8)), cols = cols, ltys = c(1,2,2))
+
+
+legFxn <- function(ii, leg.cex = .8, cols, ltys
+                   ) {
+    if(ii==1) legend('bottomleft', c('as fitted', 'no AIDS mortality'), lwd = 2, lty = 1, col = cols, bty = 'n', cex = leg.cex)
+    if(ii%in%2:3) legend('bottomleft', c('set to 0', 'as fitted', 'scaled X 10'), lwd = 2, lty = ltys,
+                         col = cols, bty = 'n', cex = leg.cex)
+    if(ii==4) legend('right', c('scaled X 1/10', 'as fitted', 'scaled X 10'), lwd = 2, lty = ltys,
+                       col = cols, bty = 'n', cex = leg.cex)
+    if(ii==5) legend('bottomleft', c('as fitted', 'std dev = 1', 'std dev = 2'), lwd = 2, lty = ltys,
+                     col = cols, bty = 'n', cex = leg.cex)
+    if(ii==6) legend('bottomleft', c('ro = 0', 'ro = 0.4', 'ro = 0.8'), lwd = 2, lty = ltys,
+                     title = 'std dv = 2',
+                     col = cols, bty = 'n', cex = leg.cex)
+}
+
+cc <- 15
 pdf(file.path(dir.figs, 'Fig 2 - Counterfactual Summary.pdf'))
 par(mfrow = c(2,3))
-for(ii in 1:6) { ## each panel
-    blocks[inds[[ii]], .(acute.sc, simj, jobnum, lab)] ## look at the meta data
-    fls <- paste0(dir.results, "/Rdatas/CF-", formatC(inds[[ii]], width=6, flag=0), ".Rdata")
-    ## load all files & combine
-    for(jj in 1:length(inds[[ii]])) {
-        load(fls[jj])
-        tsstmp <- data.table(output$tss)
-        tsstmp[, sdp:=(mm+ff)/inf.alive]
-        tsstmp <- cbind(tsstmp, data.table(t(output$pars)), simNumplot = jj)
-        if(jj==1) {
-            tss <- tsstmp
-        }else{
-            tss <- rbind(tss,tsstmp)
-        }
-    }
-    tss[, cols:= cols[simNumplot]]
-    tss[,ltys:= c(1, rep(2,10))[simNumplot]]
-    tss[, plot(yr, sdp, type = 'n', lwd = 2, xlab = '', ylab = '', bty = 'n', xlim=c(1990, 2015), ylim = c(0,1), col = cols[jj],
-                  main =mains[ii], las = 2)]
-    tss[, lines(yr, sdp, col=cols[1], lty = ltys[1]), simNumplot]
-    points(dframe.s$yr[dframe.s$group==cc1], dframe.s$psdc[dframe.s$group==cc1], pch = 19, col = 'black', cex = 1.1)     
-    if(ii==1) legend('bottomleft', c('as fitted', 'no AIDS mortality'), lwd = 2:1, lty = 1, col = c('black', 'gray'), bty = 'n', cex = leg.cex)
-    if(ii%in%2:3) legend('bottomleft', c('set to 0', 'as fitted', 'scaled X 10'), lwd = c(1,2,1), lty = c(1,1,2),
-                         col = c( 'gray', 'black', 'gray'), bty = 'n', cex = leg.cex)
-    if(ii%in%4) legend('bottomleft', c('scaled X 1/10', 'as fitted', 'scaled X 10'), lwd = c(1,2,1), lty = c(1,1,2),
-                       col = c( 'gray', 'black',  'gray'), bty = 'n', cex = leg.cex)
-    if(ii==5) legend('bottomleft', c('as fitted', 'std dev = 1', 'std dev = 2'), lwd = c(2,1,1), lty = c(1,1,2),
-                     col = c('black',  'gray', 'gray'), bty = 'n', cex = leg.cex)
-    if(ii==6) legend('bottomleft', c('ro = 0', 'ro = 0.4', 'ro = 0.8'), lwd = c(2,1,1), lty = c(1,1,2),
-                     title = 'std dv = 2',
-                     col = c('black',  'gray', 'gray'), bty = 'n', cex = leg.cex)
+for(ii in 1:6) {
+    tmp[, plot(yr, sdp, type = 'n', xlab = '', ylab = '', bty = 'n', xlim=c(1990, 2015), ylim = c(0,1), main =mains[ii], las = 2)]
+    seltmp <- get(paste0('sel',ii))
+    tmp <- tss[seltmp, .SD, nomatch=0L, on=nms, .SDcols=names(tss)][!jobnum %in% dups]
+    tmp <- merge(tmp, seltmp) ## add cols, ltys back in
+    tmp[, lines(yr, sdp, col=cols[1], lty = ltys[1], lwd=2), jobnum]
+    points(dframe.s$yr[dframe.s$group==cc], dframe.s$psdc[dframe.s$group==cc], pch = 19, col = 'black', cex = 1.1) 
+    seltmp[, legFxn(ii, cols=cols, ltys = ltys)]
 }
 graphics.off()
  
@@ -209,21 +186,21 @@ xlims[[ii]]
             lines(c(1,0), (t.arr[yrind,'mm',sel] + t.arr[yrind,'ff',sel]) / t.arr[yrind,'inf.alive',sel], col = cols[cc], type = 'l', pch = 19, lty = 1)
         }
         if(rr==2) {
-            sel <- which(cframe$group.ind==cc & cframe$simj %in% blocks$start[bltd[rr]]:blocks$end[bltd[rr]] & cframe$acute.sc==ac)
+            sel <- which(cframe$group.ind==cc & cframe$simj %in% blocksg$start[bltd[rr]]:blocksg$end[bltd[rr]] & cframe$acute.sc==ac)
             xs <- cframe$bmb.sc[sel]
             xs[cframe$bmb.sc[sel]==0] <- .05 # since on a log scale
             lines(xs, (t.arr[yrind,'mm',sel] + t.arr[yrind,'ff',sel]) / t.arr[yrind,'inf.alive',sel],
                   col = cols[cc], type = 'l', pch = 19, lty = 1)
         }
         if(rr==3) {
-            sel <- which(cframe$group.ind==cc & cframe$simj %in% blocks$start[bltd[rr]]:blocks$end[bltd[rr]] & cframe$acute.sc==ac)
+            sel <- which(cframe$group.ind==cc & cframe$simj %in% blocksg$start[bltd[rr]]:blocksg$end[bltd[rr]] & cframe$acute.sc==ac)
             xs <- cframe$bme.sc[sel]
             xs[cframe$bme.sc[sel]==0] <- .05 # since on a log scale
             lines(xs, (t.arr[yrind,'mm',sel] + t.arr[yrind,'ff',sel]) / t.arr[yrind,'inf.alive',sel],
                   col = cols[cc], type = 'l', pch = 19, lty = 1)
         }
         if(rr %in% 4) {
-            sel <- which(cframe$group.ind==cc & cframe$simj %in% (blocks$start[bltd[rr]]+1):blocks$end[bltd[rr]] & cframe$acute.sc==ac) 
+            sel <- which(cframe$group.ind==cc & cframe$simj %in% (blocksg$start[bltd[rr]]+1):blocksg$end[bltd[rr]] & cframe$acute.sc==ac) 
             lines(cframe$bmp.sc[sel], (t.arr[yrind,'mm',sel] + t.arr[yrind,'ff',sel]) / t.arr[yrind,'inf.alive',sel], col = cols[cc], type = 'l', pch = 19, lty = 1)
         }
         if(rr == 5) {
@@ -253,93 +230,5 @@ graphics.off()
 ####################################################################################################
 ####################################################################################################
 
-
-####################################################################################################
-## Figure SX for the manuscript - Heterogeneity and Assortativity
-####################################################################################################
-col.pl <- 'black'
-ac <- 7 ## acute phase RH to use in Figure
-for(one.file in c(F,T)) {
-    leg.cex <- .57
-    rmp <- colorRampPalette(c("yellow","red"))
-    hazm <- c('bmb.sc','bme.sc','bmp.sc')   ## for each route get simjob with as fitted, 0, 10 scalars; acuteRH=7, no heterogeneity
-    inds <- rbind(c(1, 91:92), c(1,91,119))
-    bltd <- c(17, 24) ## blocks to show in summary figure (no AIDS mortality, 3 routes, within-couple, heterogeneity)
-    mains <- paste0('(',LETTERS[1:2],')')
-    mains <- c('transmission risk\nheterogeneity', 'transmission risk\nassortativity')
-    tbl <- list(lab = blocks$lab[bltd], seq = list(inds[1,], inds[2,]))
-    ltys <- 1:3
-    lwds <- c(3,1.2,1.2)
-    cx <- .9
-    if(one.file) pdf(file.path(dir.figs,paste0('Figure 3 - HetAssort SummaryAc',ac,'.pdf')), w = 6.5, h = 3)
-    for(cc1 in countries) {
-        npan <- 2
-        if(!one.file)  pdf(file.path(dir.figs,paste0('Figure X - HetAssort SummaryAc ', ds.nm[cc1],' Ac',ac,'.pdf')), w = 4, h = 3)
-        layout(t(matrix(c(1:npan,npan*2+1,(npan+1):(2*npan),npan*2+1),npan+1,2)),w = c(rep(1,npan),.85))
-        par(mar = c(3,1,2,0), oma = c(1,3,0,0), cex.lab = cx, cex.axis = cx, cex.main = cx, fg = col.pl, col.axis = col.pl,
-            col.lab = col.pl, col = col.pl, col.main = col.pl)
-        for(bb in 1:npan) {
-            jst <- c(tbl$seq[[bb]])
-            js <- which(cframe$simj %in% jst)
-            js <- js[cframe$acute.sc[js]==ac & cframe$group.ind[js]==cc1] # select sims for this acute phase RH & country
-            main <- mains[bb]
-            js1 <- cframe$job[js[cframe$simj[js]==1]] # which line was as fitted? always simj=1
-            set.labs(bb,js)
-            yaxt <- ifelse(bb==1, T, F)
-            cols <- c('black', 'gray', 'gray')
-            ltys <- c(1,1,2)
-            plot.sdp.nsub(js = js, leg = leg, js1 = js1, make.pdf = F, early.yr = 1985, show.pts = show.pts, pts.group = cc1,
-                          main = main, cex.leg = .8, yaxt = yaxt, ylab = '', ltys = ltys, lwds = lwds, cols = cols,
-                          title = legtitle, browse=F, col.pl = col.pl, show.leg = F, sep.leg = F)
-            if(bb==1)   legend('bottomleft', c('as fitted', 'std dev = 1', 'std dev = 2'), lwd = c(2,1,1), lty = c(1,1,2),
-                   col = c('black',  'gray', 'gray'), bty = 'n', cex = leg.cex)
-            if(bb==2)   legend('bottomleft', c('st dev = 2', 'std dev = 2, corr = 0.4', 'std dev = 2, corr = 0.8'), lwd = c(2,1,1), lty = c(1,1,2),
-                   col = c('black',  'gray', 'gray'), bty = 'n', cex = leg.cex)
-        }
-####################################################################################################
-        ## Extract SDP's from 2008 for all scenarios to plot in 2nd row of figure
-        par(mar = c(3,1,.5,0))
-        cols <- rainbow(length(ds.nm))
-        ylab <- '' #ifelse(rr==1,'SDP','')
-        xmax <- c(1,10,10,10,2)
-        for(rr in 1:npan) {
-            yaxt <- ifelse(rr==1, T, F)
-            logd <- ''
-            if(rr==1)   xlim <- c(-.2,3.2) else xlim <- c(-.1, 1)
-            plot(1,1, type = 'n', xlim = xlim, ylim = c(0,1), bty = 'n', axes = F,
-                 xlab = '', ylab = ylab, main = '', log = logd)
-            grcol <- rgb(t(col2rgb(gray(.6), alpha = .5)),max=255)
-            segments(0,0,0,1, col = grcol, lwd = 3)
-            if(yaxt) axis(2, at = seq(0,1,l=5), las = 2) else axis(2, at = seq(0,1,l=5), labels = NA)
-            if(rr==1)   axis(1, 0:3) else axis(1, c(0,.4,.8))
-            yrind <- which(t.arr[,2,1]==2008)
-            for(cc in countries)   {
-                if(rr == 1) {
-                    sel <- which(cframe$group.ind==cc & cframe$simj %in% c(1,90:92) & cframe$acute.sc==ac)
-                    lines(cframe$het.gen.sd[sel], (t.arr[yrind,'mm',sel] + t.arr[yrind,'ff',sel]) / t.arr[yrind,'inf.alive',sel],
-                          col = cols[cc], type = 'l', pch = 19, lty = 1)
-                }else{
-                    sel <- which(cframe$group.ind==cc & cframe$simj %in% c(91,119,147) & cframe$acute.sc==ac)
-                    lines(cframe$het.gen.cor[sel], (t.arr[yrind,'mm',sel] + t.arr[yrind,'ff',sel]) / t.arr[yrind,'inf.alive',sel],
-                          col = cols[cc], type = 'l', pch = 19, lty = 1)
-                }
-            }
-            if(rr==1)       mtext('standard deviation \nof risk distribution', side = 1, line = 3, adj = .5, cex = leg.cex, outer = F)
-            if(rr==2)       mtext('inter-partner risk correlation', side = 1, line = 2, adj = .5, cex = leg.cex, outer = F)             
-        }
-        mtext('serodiscordant proportion (SDP)', side = 2, line = 2, adj = .5, cex = leg.cex, outer = T)
-        plot.new()
-        par(mar=c(0,0,0,0))
-        sel <- which(cframe$simj==1 & cframe$acute.sc==ac)
-        ord <- rev(order((t.arr[yrind,'mm',sel] + t.arr[yrind,'ff',sel]) / t.arr[yrind,'inf.alive',sel]))
-        save(ord, file=file.path(dir.results,'sdp ord.Rdata'))
-        par(xpd=NA)
-        legend('bottomleft', ds.nm[ord], col=rainbow(length(sel))[ord], lwd = 1, title = 'top to bottom', cex = .8, inset = -.1)
-        if(one.file) mtext(ds.nm[cc1], adj = 0.5, line = -2, side = 3, outer = F, cex = .75)
-        if(!one.file) dev.off()
-    }
-    if(one.file)    dev.off()
-}
-graphics.off()
 
  
